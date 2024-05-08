6ddc7a121ce28ebf8ddbc6be033cdc47bb03a738
Badral Tumursukh <23LP6834@G401-CC0100.local>
2024-05-08 09:59:38 +0800

    fix

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const io = new Server(httpServer, {
 io.on("connection", (socket) => {
   socket.on(`send-bid-message`, (message) => {
     socket.broadcast.emit("chat-message", message);
+    console.log("🚀 ~ socket.on ~ message:", message);
   });
 });
 
